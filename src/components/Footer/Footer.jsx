bbed483669dd569e3993347a43e993775ce4b8f5
Shimol-Khan <se.shimolkhan@gmail.com>
2020-04-18 22:33:42 +0800

    Finalize App

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+	return(
+		<div className={styles.container}>
+			<p>Hosted By <a href="https://github.com/Shimol-Khan" target="_blank">Shimol Khan</a></p>
+		</div>
+	)
+}
+export default Footer;
